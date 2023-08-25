ac7a33e0265fbdd04fa50c6a31276fbefe202e04
bearkim <48273075+bearkim36@users.noreply.github.com>
2023-08-25 16:05:29 +0900

    Update source.ino
    Signed-off-by: bearkim <48273075+bearkim36@users.noreply.github.com>

--- 1_week/source.ino
+++ 1_week/source.ino
@@ -1,12 +1,13 @@
+#include <ESP8266WiFi.h>
+// ESP8266 Webserver 모듈
 #include <ESP8266WebServer.h>
-#include <ESP8266Wifi.h>
 
 #define RAW_BUFFER_LENGTH 512
 #define HTTP_REST_PORT 80
 #define WIFI_RETRY_DELAY 500
 #define MAX_WIFI_INIT_RETRY 50
-const char* wifi_ssid = "haezoom_HVAC_IR";
-const char* wifi_passwd = "p@ssw0rd1@";
+const char* wifi_ssid = "haezoom5F-3";
+const char* wifi_passwd = "idnstory132";
 // HTTP Rest server 생성
 ESP8266WebServer http_rest_server(HTTP_REST_PORT);
 
@@ -38,23 +39,6 @@ void get_home() {
 // Restful API 인터페이스에 메서드 할당
 void config_rest_server_routing() {
   http_rest_server.on("/", HTTP_GET, get_home);  
-}
-
-void setup(void) {
-   Serial.begin(115200);
-
-  // 와이파이 연결 시도
-  if (init_wifi() == WL_CONNECTED) {
-    Serial.print("Connetted to ");
-    Serial.print(wifi_ssid);
-    Serial.print("--- IP: ");
-    Serial.println(WiFi.localIP());
-  }
-  else {
-    Serial.print("Error connecting to: ");
-    Serial.println(wifi_ssid);
-  }
-           
 }
 
 void setup(void) {
